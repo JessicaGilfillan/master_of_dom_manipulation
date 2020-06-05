3c6ea9b7e9def226b4cd68f49740e84b55983eae
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-06-05 17:25:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+
+//1.) Add a new paragraph to the page
+
+//2.) Remove the class attribute on the header element 
+
+//3.) Change the src attribute in the image element to 'cat2.jpg'
+
+//4.) Remove the footer element 
+
+//5.) Add an h3 into the header 
